5aad73c27f8bdf01b7c3b0c6612a95729f25cfbd
Eimantas Žilevičius <eimantaszilevicius@gmail.com>
2022-11-20 11:07:32 +0200

    Hamburgeris

--- municipalities/src/App.jsx
+++ municipalities/src/App.jsx
@@ -1,15 +1,9 @@
 import "./App.scss";
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-  Navigate,
-  useNavigate,
-} from "react-router-dom";
-import Nav from "./Components/Nav";
-import { login, logout, authConfig } from "./Functions/auth";
-import { useState, useEffect, useCallback } from "react";
-import axios from "axios";
+import "bootstrap/dist/js/bootstrap.bundle";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ShowNav from "./Functions/showNav";
+import RequireAuth from "./Functions/requireAuth";
+import { useState, useCallback } from "react";
 import DataContext from "./Contexts/DataContext";
 import { v4 as uuidv4 } from "uuid";
 import Messages from "./Components/Messages";
@@ -19,7 +13,12 @@ import FirstCrud from "./Components/firstCrud/Main";
 import SecondCrud from "./Components/secondCrud/Main";
 import ThirdCrud from "./Components/thirdCrud/Main";
 import FourthCrud from "./Components/fourthCrud/Main";
+
 // import RegisterPage from "./Components/register/Main";
+
+// Logins
+import LoginPage from "./Components/logins/loginPage";
+import LogoutPage from "./Components/logins/logoutPage";
 
 function App() {
   const [roleChange, setRoleChange] = useState(Date.now());
@@ -85,103 +84,6 @@ function App() {
       </BrowserRouter>
     </DataContext.Provider>
   );
-}
-
-function ShowNav({ roleChange }) {
-  const [status, setStatus] = useState(1);
-  useEffect(() => {
-    axios
-      .get("http://localhost:3003/login-check?role=admin", authConfig())
-      .then((res) => {
-        setStatus(res.data.status);
-      });
-  }, [roleChange]);
-  return <Nav status={status} />;
-}
-
-function RequireAuth({ children, role }) {
-  const [view, setView] = useState(<h2>Please wait...</h2>);
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:3003/login-check?role=" + role, authConfig())
-      .then((res) => {
-        if ("ok" === res.data.msg) {
-          setView(children);
-        } else if (res.data.status === 2) {
-          setView(<h2>Unauthorize...</h2>);
-        } else {
-          setView(<Navigate to="/" replace />);
-        }
-      });
-  }, [children, role]);
-
-  return view;
-}
-
-function LoginPage({ setRoleChange }) {
-  const navigate = useNavigate();
-
-  const [user, setUser] = useState("");
-  const [pass, setPass] = useState("");
-
-  const doLogin = () => {
-    axios.post("http://localhost:3003/login", { user, pass }).then((res) => {
-      setRoleChange(Date.now());
-      if ("ok" === res.data.msg) {
-        login(res.data.key);
-        navigate("/", { replace: true });
-      }
-    });
-  };
-
-  return (
-    <div className="container">
-      <div className="row justify-content-center">
-        <div className="col-md-6 col-lg-4 ">
-          <div className="card m-4">
-            <h5 className="card-header">Login</h5>
-            <div className="card-body">
-              <div className="mb-3">
-                <label className="form-label">name</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  value={user}
-                  onChange={(e) => setUser(e.target.value)}
-                />
-              </div>
-              <div className="mb-3">
-                <label className="form-label">password</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  value={pass}
-                  onChange={(e) => setPass(e.target.value)}
-                />
-              </div>
-              <button
-                onClick={doLogin}
-                type="button"
-                className="btn btn-outline-success"
-              >
-                Login
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function LogoutPage({ setRoleChange }) {
-  useEffect(() => {
-    logout();
-    setRoleChange(Date.now());
-  }, [setRoleChange]);
-
-  return <Navigate to="/" replace />;
 }
 
 export default App;
--- municipalities/src/Components/Nav.jsx
+++ municipalities/src/Components/Nav.jsx
@@ -7,10 +7,20 @@ function Nav({ status }) {
         <div className="col-12">
           <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-              {/* App name!!!!!!!!!!!!!!!!!!!!!!!!!!! */}
-              <span className="navbar-brand">StyleWithStyle</span>
-              <div>
-                <div className="navbar-nav">
+              <div className="navbar-brand">StyleWithStyle</div>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
                   {status === 2 || status === 3 || status === 4 ? (
                     <NavLink
                       to="/"
@@ -67,7 +77,7 @@ function Nav({ status }) {
                       Login
                     </NavLink>
                   ) : null}
-                </div>
+                </ul>
               </div>
             </div>
           </nav>
